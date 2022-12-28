ec1e5a256a67749fbdfd4a08bc9ab7fe81a71669
Jurand <untech.untech@gmail.com>
2022-12-29 00:08:10 +0100

    mern

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,34 @@
 require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
 
-//express app
+// express app
 const app = express();
 
-//middleware
-app.use(express.json()); // - giving access to the body
+// middleware
+app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
-//routes
-// app.get('/', (req, res) => {
-//   res.json({ mssg: 'Welcome to the app' });
-// });
-
+// routes
 app.use('/api/workouts', workoutRoutes);
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-  console.log(`listening on port ${process.env.PORT}`);
-});
+// connect to db
+mongoose
+  .set('strictQuery', false)
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('connected to database');
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
